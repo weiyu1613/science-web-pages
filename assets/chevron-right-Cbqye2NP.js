aabb0ba9905ef68d9323905a05437d1dd07f426c
Vivid <7477127@qq.com>
2026-06-25 03:27:57 +0800

    deploy: v2.3 Canvas批量修复

---
+++ assets/chevron-right-Cbqye2NP.js
@@ -0,0 +1,1 @@
+import{u as e}from"./proxy-6CaK6RIN.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
---
+++ assets/clock-BcsqQzYh.js
@@ -0,0 +1,1 @@
+import{u as e}from"./proxy-6CaK6RIN.js";var t=e(`clock`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`M12 6v6l4 2`,key:`mmk7yg`}]]);export{t};
